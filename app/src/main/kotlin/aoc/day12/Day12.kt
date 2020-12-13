626c6d5f37c138aaabf35f842a5f77f8d635b3c5
RileyMichael <riley.mcgee@centurylink.com>
2020-12-12 21:57:48 -0600

    day 12 part 2

--- app/src/main/kotlin/aoc/day12/Day12.kt
+++ app/src/main/kotlin/aoc/day12/Day12.kt
@@ -8,36 +8,51 @@ fun main() {
 
     val part1 = part1(input)
     println("Part 1: $part1")
+    val part2 = part2(input)
+    println("Part 2: $part2")
 }
 
 fun part1(input: List<String>): Int {
     var facing = 90
-    var position = Position(0, 0)
+    var ship = Position(0, 0)
     input.parseInstructions().forEach { (action, value) ->
         when (action) {
-            Action.NORTH -> position += Position(0, value)
-            Action.SOUTH -> position += Position(0, value.unaryMinus())
-            Action.EAST -> position += Position(value, 0)
-            Action.WEST -> position += Position(value.unaryMinus(), 0)
-            Action.LEFT -> {
-                facing = (facing - value) % 360
-                if (facing < 0) facing += 360
-            }
-            Action.RIGHT -> {
-                facing = (facing + value) % 360
-                if (facing < 0) facing += 360
-            }
+            Action.NORTH -> ship += Position(0, value)
+            Action.SOUTH -> ship += Position(0, value.unaryMinus())
+            Action.EAST -> ship += Position(value, 0)
+            Action.WEST -> ship += Position(value.unaryMinus(), 0)
+            Action.LEFT -> facing = (facing - value).normalize()
+            Action.RIGHT -> facing = (facing + value).normalize()
             Action.FORWARD -> when (facing) {
-                0 -> { position += Position(0, value) }
-                90 -> { position += Position(value, 0) }
-                180 -> { position += Position(0, value.unaryMinus()) }
-                270 -> { position += Position(value.unaryMinus(), 0) }
-                else -> {} // ?
+                0 -> ship += Position(0, value)
+                90 -> ship += Position(value, 0)
+                180 -> ship += Position(0, value.unaryMinus())
+                270 -> ship += Position(value.unaryMinus(), 0)
+                else -> { } // ?
             }
         }
     }
 
-    return abs(position.x) + abs(position.y)
+    return abs(ship.x) + abs(ship.y)
+}
+
+fun part2(input: List<String>): Int {
+    var ship = Position(0, 0)
+    var waypoint = Position(10, 1)
+
+    input.parseInstructions().forEach { (action, value) ->
+        when (action) {
+            Action.NORTH -> waypoint += Position(0, value)
+            Action.SOUTH -> waypoint += Position(0, value.unaryMinus())
+            Action.EAST -> waypoint += Position(value, 0)
+            Action.WEST -> waypoint += Position(value.unaryMinus(), 0)
+            Action.LEFT -> waypoint = waypoint.rotate(value.unaryMinus())
+            Action.RIGHT -> waypoint = waypoint.rotate(value)
+            Action.FORWARD -> ship += waypoint * value
+        }
+    }
+
+    return abs(ship.x) + abs(ship.y)
 }
 
 typealias Instruction = Pair<Action, Int>
@@ -48,9 +63,23 @@ fun List<String>.parseInstructions(): List<Instruction> {
     }
 }
 
+fun Int.normalize(): Int {
+    var angle = this % 360
+    if (angle < 0) angle += 360
+    return angle
+}
+
 data class Position(val x: Int, val y: Int) {
     operator fun plus(pos: Position) = Position(x + pos.x, y + pos.y)
     operator fun minus(pos: Position) = Position(x - pos.x, y - pos.y)
+    operator fun times(i: Int) = Position(x * i, y * i)
+
+    fun rotate(degrees: Int) = when (degrees.normalize()) {
+        90 -> Position(y, -x)
+        180 -> Position(-x, -y)
+        270 -> Position(-y, x)
+        else -> this // ?
+    }
 }
 
 enum class Action(val char: Char) {
@@ -64,7 +93,6 @@ enum class Action(val char: Char) {
 
     companion object {
         private val byChar = values().associateBy { it.char }
-
         fun fromChar(c: Char) = byChar[c]!!
     }
 }
--- app/src/test/kotlin/aoc/day12/Day12Test.kt
+++ app/src/test/kotlin/aoc/day12/Day12Test.kt
@@ -6,14 +6,23 @@ import org.junit.jupiter.api.Test
 class Day12Test {
     @Test
     fun part1() {
-        val input = listOf(
+        val actual = part1(input)
+        Assertions.assertEquals(25, actual)
+    }
+
+    @Test
+    fun part2() {
+        val actual = part2(input)
+        Assertions.assertEquals(286, actual)
+    }
+
+    companion object {
+        private val input = listOf(
             "F10",
             "N3",
             "F7",
             "R90",
             "F11",
         )
-        val actual = part1(input)
-        Assertions.assertEquals(25, actual)
     }
 }
